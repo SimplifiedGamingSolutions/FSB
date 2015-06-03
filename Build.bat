a8a818bf28ee15e773b1e4c1b13008a7118a1650
David <longlostbro@gmail.com>
2015-06-02 18:01:26 -0600

    build

--- Build.bat
+++ Build.bat
@@ -1,4 +1,5 @@
 call gradlew build
 copy /y build\libs\*.jar ..\SimplifiedGamingSolutions\MCMA\Server\mods\
 copy /y build\libs\*.jar %appdata%\.minecraft\mods\
+copy /y build\libs\*.jar C:\MCMA\Server\mods
 PAUSE
